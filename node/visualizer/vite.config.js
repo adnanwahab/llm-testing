85c282c02b35d1d8801da79c0bec7f42b7bf69d5
Your Name <you@example.com>
2023-08-27 08:29:28 -0700

    asdfasfd

--- node/visualizer/vite.config.js
+++ node/visualizer/vite.config.js
@@ -1,46 +1,19 @@
+import react from '@vitejs/plugin-react'
+
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
-
-// export default defineConfig({
-//         root: 'src/',
-//     publicDir: '../static/',
-//     base: './',
-//     server: {
-//         proxy: { "/api/v1": "http://localhost:5000",
-
-//                 "/api/yt-click": "http://localhost:5000/dl-yt"
-    
-//     }
-//     //   proxy: {
-//     //     '/api/v1': {
-//     //       target: 'http://localhost:5000/api/v1',
-//     //       changeOrigin: true,
-//     //       secure: tls.VERSION_TLSv1_2,
-//     //       // Additional configurations if needed
-//     //     },
-//     //   },
-//     },
-//   });
-
 export default {
+    plugins:
+    [
+        react()
+    ],
     root: 'src/',
-    publicDir: '../static/',
+    publicDir: "../public/",
     base: './',
-
     server:
     {
-        https: true,
         host: true,
-        open: !isCodeSandbox,
-        cors: true,
-        proxy: {
-            '/api': {
-                 target: 'https://localhost:5000',
-                 changeOrigin: true,
-                 secure: false,      
-                 ws: true,
-             }
-        },
+        open: !isCodeSandbox // Open if it's not a CodeSandbox
     },
     build:
     {
